442d813d68f4c60c8aed1ceed3b674cd09b1750d
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-04-02 18:20:08 +0200

    currency calculator modified

--- js/script.js
+++ js/script.js
@@ -1,60 +1,57 @@
 {
-    const changeCurrency = (currency, amount, currentValue) => {
+    const changeCurrency = (currency, amount) => {
     
         const USD = 3.97;
         const EUR = 4.52;
         const GBP = 5.41;
-    
+
         switch (currency) {
+
             case "USD":
-                currentValue.value = USD;
-                return amount/USD; 
+                return amount/USD;
             case "EUR":
-                currentValue.value = EUR;
                 return amount/EUR;
             case "GBP":
-                currentValue.value = GBP;
                 return amount/GBP;
         };
     }
     
-    const updateTextElement = (currency, amount, changedCurrency, updateElement, currentValue) => {
+    const updateTextElement = (currency, amount, changedCurrency, updateElement) => {
         updateElement.innerHTML = 
         `
-        The current rate ${currency} is: <strong>${currentValue.value}</strong>.<br>
         After exchange <strong>${amount}</strong> PLN you will receive <strong>${changedCurrency.toFixed(2)}</strong> ${currency}.
         `;
     }
     
-    const deleteInputs = (currencyInput, updateElement, amountInput, currentValue) => {
+    const deleteInputs = (currencyInput, updateElement, amountInput) => {
         currencyInput.addEventListener("input", (event) => {
             event.preventDefault();
-            currentValue.value = null;
             updateElement.innerHTML = "";
             amountInput.value = null;
         })
     }
     
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    
+        const currencyInput = document.querySelector(".js-currency");
+        const amountInput = document.querySelector(".js-amount");
+        const updateElement = document.querySelector(".js-updateElement");
+
+        const currency = currencyInput.value;
+        const amount = +amountInput.value;
+    
+        const changedCurrency = changeCurrency(currency, amount);
+        
+        updateTextElement(currency, amount, changedCurrency, updateElement);
+        deleteInputs(currencyInput, updateElement, amountInput);
+    }
+
     const init = () => {
     
         const formElement = document.querySelector(".js-form");
     
-        formElement.addEventListener("submit", (event) => {
-            event.preventDefault();
-        
-            const currencyInput = document.querySelector(".js-currency");
-            const amountInput = document.querySelector(".js-amount");
-            const currentValue = document.querySelector(".js-currentValue");
-            const updateElement = document.querySelector(".js-updateElement");
-    
-            const currency = currencyInput.value;
-            const amount = +amountInput.value;
-        
-            const changedCurrency = changeCurrency(currency, amount, currentValue);
-            
-            updateTextElement(currency, amount, changedCurrency, updateElement, currentValue);
-            deleteInputs(currencyInput, updateElement, amountInput, currentValue);
-        })
+        formElement.addEventListener("submit", onFormSubmit);
     }
     
     init();
